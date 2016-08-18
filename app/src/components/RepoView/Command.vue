9c320ad0f56af394c6669981b230d90e7aa52d00
Michael Kühnel <mail@michael-kuehnel.de>
2016-08-18 23:10:38 +0200

    Add titles to script buttons

--- app/src/components/RepoView/Command.vue
+++ app/src/components/RepoView/Command.vue
@@ -41,19 +41,19 @@
         {{ script.name }}
       </div>
       <div class="{{ scriptClass }}--actions">
-        <button type="button" class="o-icon" v-if="script.docs" v-open-external="script.docs">
+        <button title="Open docs in browser" type="button" class="o-icon" v-if="script.docs" v-open-external="script.docs">
           <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M19 4H5c-1.11 0-2 .9-2 2v12c0 1.1.89 2 2 2h4v-2H5V8h14v10h-4v2h4c1.1 0 2-.9 2-2V6c0-1.1-.89-2-2-2zm-7 6l-4 4h3v6h2v-6h3l-4-4z"/>
           </svg>
         </button>
-        <button type="button" class="o-icon" @click="toggleVisibility()">
+        <button title="Show command" type="button" class="o-icon" @click="toggleVisibility()">
           <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"/>
           </svg>
         </button>
-        <button type="button" class="o-icon" @click="runScript( script, { isCustom : isCustom } )" aria-label="Run script" data-run-script>
+        <button title="Run" type="button" class="o-icon" @click="runScript( script, { isCustom : isCustom } )" aria-label="Run script" data-run-script>
           <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0h24v24H0z" fill="none"/>
             <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
